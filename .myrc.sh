391dfd0295a169c8ecd6c7777dad3edc02107830
Andrew Nee <ndrewnee@gmail.com>
2017-07-04 17:09:40 +0300

    Added helper function replace

--- .myrc.sh
+++ .myrc.sh
@@ -6,3 +6,17 @@ export PATH="$GOPATH/bin/:/usr/local/Cellar/openvpn/2.4.1/sbin/:/usr/local/opt/g
 export NVM_DIR="$HOME/.nvm"
 . "/usr/local/opt/nvm/nvm.sh"
 export EDITOR=vim
+
+# TODO Refator
+function replace {
+    if [ $# -ne 2 ]
+      then
+        echo "Usage: replace <find> <replace>"
+        return
+    fi
+
+    from="$1"
+    to="$2"
+    
+    ack -l ''$from'' | xargs perl -pi -E 's/'$from'/'$to'/g'
+}
