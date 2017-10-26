e6dedfdc8e8cb95b9c16df1d05b7ba65543bcb73
Andrew Nee <ndrewnee@gmail.com>
2017-10-26 22:17:55 +0300

    Updated .myrc.sh
    - Added gvm settings
    - Added python
    - Removed unused

--- .myrc.sh
+++ .myrc.sh
@@ -3,16 +3,26 @@ alias l='ls -G'
 alias ll='ls -lGaf'
 alias vim='/Applications/MacVim.app/Contents/MacOS/Vim'
 alias typora="open -a typora"
+# default editor
+export EDITOR=vim
 
-# golang
+export PATH="/usr/local/bin:/usr/local/sbin:$PATH"
+
+# gvm - golang version manager
+[[ -s "/Users/ndrewnee/.gvm/scripts/gvm" ]] && source "/Users/ndrewnee/.gvm/scripts/gvm"
+export GOROOT_BOOTSTRAP=$(go env GOROOT)
+
 export GOPATH=$HOME/Projects/go
+
+export PATH="$GOPATH/bin:$PATH"
+export PATH="/usr/local/opt/go/libexec/bin:$PATH"
+export PATH="/usr/local/Cellar/openvpn/2.4.1/sbin:$PATH"
 export PATH="/usr/local/Cellar/make/4.2.1_1/bin:$PATH"
-export PATH="$GOPATH/bin/:/usr/local/Cellar/openvpn/2.4.1/sbin/:/usr/local/opt/go/libexec/bin:$PATH"
+export PATH="/usr/local/opt/python/libexec/bin:$PATH"
+
 # for nvm
 export NVM_DIR="$HOME/.nvm"
 . "/usr/local/opt/nvm/nvm.sh"
-# default editor
-export EDITOR=vim
 
 # rbenv install
 export PATH="$HOME/.rbenv/bin:$PATH"
@@ -21,20 +31,3 @@ eval "$(rbenv init -)"
 # for russian coding in Wine
 export LC_COLLATE=ru_RU.UTF-8
 export LC_CTYPE=ru_RU.UTF-8
-
-# telegram binary
-export PATH="/Users/ndrewnee/Projects/tg/bin:$PATH"
-
-# find and replace in terminal
-function replace {
-    if [ $# -ne 2 ]
-      then
-        echo "Usage: replace <find> <replace>"
-        return
-    fi
-
-    from="$1"
-    to="$2"
-
-    ack -l ''$from'' | xargs perl -pi -E 's/'$from'/'$to'/g'
-}
